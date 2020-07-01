ed9fe95db69473e5074bd48f24f1bab24e56c5a9
Ludvig Lundberg <ludvig.lundberg@hotmail.com>
2020-07-01 22:17:24 +0200

    Added list_dags to main function

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/LudvigLundberg/go-airflow/cli"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		panic("No argument passed")
+	}
+	if len(args) == 1 {
+		switch args[0] {
+		case "list_dags":
+			listDags()
+			break
+		default:
+			panic("Command not supported: " + args[0])
+		}
+	}
+}
+
+func listDags() {
+	dags, err := cli.ListDags()
+
+	if err != nil {
+		panic("Error: " + err.Error())
+	}
+
+	for _, dag := range dags {
+		fmt.Println(dag)
+	}
+}
